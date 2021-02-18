87096cb3303fb71a372b77fabfdbfcac28da8584
Fernando <luiz-fernando@outlook.com>
2021-02-18 14:45:43 -0300

    Criada função para buscar avatar de usuário no github
    close #9

---
+++ github_api.py
@@ -0,0 +1,10 @@
+import requests
+
+def buscar_avatar(usuario):
+    url = f'https://api.github.com/users/{usuario}'
+    response = requests.get(url)
+    return response.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('lfstos'))
