4c0aee3d6b629820cf2fe4c63725d2b716197254
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-09-20 20:20:42 -0300

    :rocket: Simulando model Usuário

---
+++ libpythonpro/spam/modelos.py
@@ -0,0 +1,6 @@
+class Usuario:
+    contador = 0
+    def __init__(self, nome):
+        # Adicinando o nome que recebemos como atributo na variável nome
+        self.nome = nome
+        self.id = None
