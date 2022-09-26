d2ece5ff7e7b0c28621d7e89fee28d8689b0ecc7
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-09-26 19:31:56 -0300

    :rocket: Adicionado parâmetro email ao modelo Usuario

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -6,13 +6,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Fernando')
+    usuario = Usuario(nome='Fernando', email="luiz-fernando@outlook.com")
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Fernando'), Usuario(nome='Manon')]
+    usuarios = [
+        Usuario(nome='Fernando', email="luiz-fernando@outlook.com"),
+        Usuario(nome='Manon', email="santos.manon@hotmail.com")
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
