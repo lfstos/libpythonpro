64e7a1d7af6cf359dff117aa0b62a6c6f2527309
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-09-26 19:32:48 -0300

    :rocket: Adicionado a fixture do pytest.mark.parametrize

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,28 @@
+import pytest
+from libpythonpro.spam.modelos import Usuario
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
 
-
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios', [
+        [
+            Usuario(nome='Fernando', email="luiz-fernando@outlook.com"),
+            Usuario(nome='Manon', email="santos.manon@hotmail.com")
+        ],
+        [
+            Usuario(nome='Fernando', email="luiz-fernando@outlook.com"),
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'luiz-fernando@outlook.com',
         'Curso Dev Pro',
         'Confira os módulos Fantásticos'
     )
+    assert len(usuarios) == enviador.qtd_emails_enviados
