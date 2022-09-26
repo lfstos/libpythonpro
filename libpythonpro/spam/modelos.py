950a81e353f269e130c1a3488ff173ea8c0a074f
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-09-26 19:29:11 -0300

    :rocket: Criado modelo Usuario

--- libpythonpro/spam/modelos.py
+++ libpythonpro/spam/modelos.py
@@ -1,6 +1,7 @@
 class Usuario:
     contador = 0
-    def __init__(self, nome):
+    def __init__(self, nome, email):
         # Adicinando o nome que recebemos como atributo na variável nome
         self.nome = nome
+        self.email=email
         self.id = None
