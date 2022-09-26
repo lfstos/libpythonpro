5eda20cb8109036e21bc9fa1f83d061b204b05d2
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-09-26 19:27:43 -0300

    :rocket: Criado atributo qtd_emails_enviado

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,7 +1,9 @@
 class Enviador:
+    qtd_emails_enviados = 0
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email de remetente inválido: {remetente}')
+        self.qtd_emails_enviados += 1
         return remetente
 
 
